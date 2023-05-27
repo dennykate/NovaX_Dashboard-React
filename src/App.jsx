014f30a8209ae883b7cf1040cb60a51c00663edc
Denny_Kate <dennykate22@gmail.com>
2023-05-27 22:38:16 +0630

    change route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import "./App.css"
-import { Route, Routes } from 'react-router-dom'
-import { Dashboard, Login, Register } from './pages'
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Dashboard, Login, Register } from "./pages";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Dashboard/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/registetr' element={<Register/>}/>
-
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registetr" element={<Register />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
